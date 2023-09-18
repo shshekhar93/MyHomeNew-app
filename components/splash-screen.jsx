124a2e160c3a8722b8f30cf86ac44918a0c2eacb
Shashi Shekhar <shshekhar93@gmail.com>
2023-09-19 03:39:02 +0530

    Add web splash screen

---
+++ components/splash-screen.jsx
@@ -0,0 +1,29 @@
+import React, { useContext } from 'react';
+import { Image, View } from 'react-native';
+import { ThemeContext } from '../lib/utils';
+import splashSrc from '../assets/splash.png';
+
+export default function SplashScreen() {
+  const theme = useContext(ThemeContext);
+
+  return (
+    <View
+      style={{
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: theme.PAGE_BACKGROUND,
+        width: '100vw',
+        height: '100vh',
+      }}
+    >
+      <Image
+        source={splashSrc}
+        style={{
+          width: 200,
+          height: 200,
+        }}
+      />
+    </View>
+  );
+}
