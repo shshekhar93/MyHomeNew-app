4fef07996fc424bd838e0ca6a277d2150600ee4a
Shashi Shekhar <shshekhar93@gmail.com>
2025-12-30 11:41:19 +0000

    Fix scroll issue

--- components/common/base-page-view.tsx
+++ components/common/base-page-view.tsx
@@ -1,11 +1,19 @@
-import React, { useContext } from 'react';
+import React, { useContext, useMemo } from 'react';
+import { Dimensions } from 'react-native';
+
 import { ScrollView } from 'react-native';
 import { ThemeContext } from '../../lib/utils';
 
 const DEFAULT_PADDING = 15;
 
+
 export default function BasePageView({ style = {}, children, ...restProps }) {
   const theme = useContext(ThemeContext);
+  
+  const maxHeight = useMemo(() => {
+    const { height } = Dimensions.get('window')
+return height - 64;
+  }, []);
 
   return (
     <ScrollView
@@ -14,6 +22,7 @@ export default function BasePageView({ style = {}, children, ...restProps }) {
         padding: DEFAULT_PADDING,
         backgroundColor: theme.PAGE_BACKGROUND,
         minHeight: 'auto',
+        maxHeight,
         ...style,
       }}
       {...restProps}
